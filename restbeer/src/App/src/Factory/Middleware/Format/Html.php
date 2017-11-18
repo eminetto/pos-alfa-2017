30ef26208066d9607208062f67b929cfdb9ba216
Elton Minetto <eminetto@gmail.com>
2017-11-18 15:17:47 -0200

    Resolve bug do Zend View

--- restbeer/src/App/src/Factory/Middleware/Format/Html.php
+++ restbeer/src/App/src/Factory/Middleware/Format/Html.php
@@ -5,18 +5,13 @@ namespace App\Factory\Middleware\Format;
 use Interop\Container\ContainerInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
 use App\Middleware\Format\Html as HtmlMiddleware;
-use Zend\Expressive\ZendView\ZendViewRenderer;
 
 class Html
 {
     public function __invoke(ContainerInterface $container)
     {
-        $renderer = new ZendViewRenderer();
-        // print_r($renderer);
-        // exit;
         return new HtmlMiddleware(
-            // $container->get(TemplateRendererInterface::class)
-            $renderer
+            $container->get(TemplateRendererInterface::class)
         );
     }
 }
