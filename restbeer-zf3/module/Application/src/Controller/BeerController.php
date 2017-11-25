641190f51631b261a8c51d790da07ca94ec414e6
Elton Minetto <eminetto@gmail.com>
2017-11-25 14:36:39 -0200

    Adiciona uma cerveja usando Forms

--- restbeer-zf3/module/Application/src/Controller/BeerController.php
+++ restbeer-zf3/module/Application/src/Controller/BeerController.php
@@ -32,4 +32,35 @@ class BeerController extends AbstractActionController
         $this->flashMessenger()->addMessage("Cerveja excluida");
         return $this->redirect()->toRoute('beer');
     }
+
+    public function createAction()
+    {
+        $form = new \Application\Form\Beer;
+        $form->setAttribute('action', '/beer/create');
+        $request = $this->getRequest();
+        /* se a requisição é post os dados foram enviados via formulário*/
+        if ($request->isPost()) {
+            $beer = new \Application\Model\Beer;
+            /* configura a validação do formulário com os filtros e validators da entidade*/
+            $form->setInputFilter($beer->getInputFilter());
+            /* preenche o formulário com os dados que o usuário digitou na tela*/
+            $form->setData($request->getPost());
+            /* faz a validação do formulário*/
+            if ($form->isValid()) {
+                /* pega os dados validados e filtrados */
+                $data = $form->getData();
+                unset($data['send']);
+                /* salva a cerveja*/
+                $this->tableGateway->insert($data);
+                $this->flashMessenger()->addMessage("Cerveja incluída");
+                /* redireciona para a página inicial que mostra todas as cervejas*/
+                return $this->redirect()->toUrl('/beer');
+            }
+        }
+        $view = new ViewModel(['form' => $form]);
+        $view->setTemplate('application/beer/save.phtml');
+
+        return $view;
+    }
+
 }
